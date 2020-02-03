5270ca6bb3304c180a85212f52479ff0b2199a73
Samuel Tijani <omalitijani@gmail.com>
2020-02-03 10:22:10 +0100

    Simple decimal to binary conversion file

---
+++ decimaltobinary.py
@@ -0,0 +1,18 @@
+"""
+File: decimaltobinary.py
+Converts a decimal integer to a string of bits.
+"""
+
+decimal = eval(input("Enter a decimal integer: "))
+if decimal == 0:
+    print(0)
+else:
+    print("Quotient Remainder Binary")
+    bstring = ""
+    while decimal > 0:
+        remainder = decimal % 2
+        decimal = int(decimal / 2)
+        bstring = str(remainder) + bstring
+        print("%5d%8d%12s" % (decimal, remainder, bstring))
+    print("The binary representation is", bstring)
+ 
